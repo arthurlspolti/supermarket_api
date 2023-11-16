060c92ef9a0c862dbec36aaaed9533b1ccfe4e97
Arthur <arthurlspolti2@gmail.com>
2023-11-16 16:19:43 -0300

    Update route promotions

--- index.js
+++ index.js
@@ -72,8 +72,35 @@ const buscarGrupos = async (prisma, idsProdutos) => {
 };
 
 // Função para buscar produtos em promoção
-const buscarProdutosPromocao = async (prisma, produto, idsProdutos) => {
-  return await prisma.products.findMany({
+const buscarProdutosAdicionais = async (prisma, produtos, idsProdutos) => {
+  let produtosAdicionais = [];
+  let idsAdicionais = []; // Novo array para armazenar os IDs dos produtos adicionais
+  for (let id of idsProdutos) {
+    const produto = produtos.find((produto) => produto.id === id);
+    if (produto && produto.Category) {
+      const produtosPromocao = await buscarProdutosPromocao(
+        prisma,
+        produto,
+        idsProdutos,
+        idsAdicionais // Passa o array de IDs para a função buscarProdutosPromocao
+      );
+      produtosAdicionais = [...produtosAdicionais, ...produtosPromocao];
+      idsAdicionais = [
+        ...idsAdicionais,
+        ...produtosPromocao.map((produto) => produto.id),
+      ]; // Adiciona os IDs dos novos produtos ao array
+    }
+  }
+  return produtosAdicionais;
+};
+
+const buscarProdutosPromocao = async (
+  prisma,
+  produto,
+  idsProdutos,
+  idsAdicionais
+) => {
+  const produtosPromocao = await prisma.products.findMany({
     where: {
       Category: {
         id: produto.Category.id,
@@ -83,30 +110,14 @@ const buscarProdutosPromocao = async (prisma, produto, idsProdutos) => {
         gt: 0,
       },
       id: {
-        notIn: idsProdutos,
+        notIn: [...idsProdutos, ...idsAdicionais], // Verifica se o produto já foi adicionado
       },
     },
     include: {
       Category: true,
     },
   });
-};
-
-// Função para buscar produtos adicionais
-const buscarProdutosAdicionais = async (prisma, produtos, idsProdutos) => {
-  let produtosAdicionais = [];
-  for (let id of idsProdutos) {
-    const produto = produtos.find((produto) => produto.id === id);
-    if (produto && produto.Category) {
-      const produtosPromocao = await buscarProdutosPromocao(
-        prisma,
-        produto,
-        idsProdutos
-      );
-      produtosAdicionais = [...produtosAdicionais, ...produtosPromocao];
-    }
-  }
-  return produtosAdicionais;
+  return produtosPromocao;
 };
 
 // Função para agrupar produtos
