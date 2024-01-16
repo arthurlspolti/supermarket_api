44c71a16dd3c74b2b75915ba1ae94a4bb9382959
Arthur <arthurlspolti2@gmail.com>
2024-01-16 12:17:12 -0300

    Fix: shoppingRoute error

--- src/services/shoppingRouteService.js
+++ src/services/shoppingRouteService.js
@@ -1,5 +1,52 @@
 import ShoppingRoute from "../models/shoppingRoute.js";
 
+// Função para buscar produtos em promoção
+const buscarProdutosAdicionais = async (produtos, idsProdutos) => {
+  let produtosAdicionais = [];
+  let idsAdicionais = []; // Novo array para armazenar os IDs dos produtos adicionais
+  for (let id of idsProdutos) {
+    const produto = produtos.find((produto) => produto.id === id);
+    if (produto && produto.Category) {
+      const produtosPromocao = await buscarProdutosPromocao(
+        produto,
+        idsProdutos,
+        idsAdicionais // Passa o array de IDs para a função buscarProdutosPromocao
+      );
+      produtosAdicionais = [...produtosAdicionais, ...produtosPromocao];
+      idsAdicionais = [
+        ...idsAdicionais,
+        ...produtosPromocao.map((produto) => produto.id),
+      ]; // Adiciona os IDs dos novos produtos ao array
+    }
+  }
+  return produtosAdicionais;
+};
+
+// Função para agrupar produtos
+const agruparProdutos = (todosProdutos, idsProdutos, todasCategorias) => {
+  const agrupados = {};
+  // Inicializar todas as categorias com listas de produtos vazias
+  todasCategorias.forEach((categoria) => {
+    agrupados[categoria.localization] = {
+      AisleNumber: categoria.localization,
+      products: [],
+      promotions: [],
+    };
+  });
+  todosProdutos.forEach((produto) => {
+    const chave = produto.Category
+      ? produto.Category.localization
+      : "Sem categoria";
+    if (idsProdutos.includes(produto.id)) {
+      agrupados[chave].products.push(produto);
+    } else {
+      agrupados[chave].promotions.push(produto);
+    }
+  });
+  // Converte o objeto agrupado em um array
+  return Object.values(agrupados);
+};
+
 const shoppingRouteService = {
   getShoppingRoutes: async (idsProdutos) => {
     const produtos = await ShoppingRoute.findProductsByIds(idsProdutos);
